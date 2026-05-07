cd1c5a783a293c08730e49acd4d70f1902bfa9d5
sporteiro <seblash@gmail.com>
2026-05-07 16:50:23 +0100

    corrected styles in old files

--- desactivada.php
+++ desactivada.php
@@ -1,8 +1,22 @@
-<div style="width: 100%; height: 100%; background-image:url(imagenes/fondousuario.png); position: fixed;">
-<div style="margin:20%; padding: 30px; background-color: #063;">
-Tu cuenta no esta activa<br />
-Para poder aceder a tu cuenta, es necesario que la actives desde tu correo electronico<br />
-Si no encontras el correo, revisa tu bandeja de correo no deseado<br /><br /><br />
-<a href="index.php" class="botoneschicos">cerrar </a>
-</div>
-</div>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuenta desactivada - Profeta Mundial</title>
+    <link href="estilo.css" rel="stylesheet" type="text/css">
+    <link href="css/mundial2026.css" rel="stylesheet" type="text/css">
+</head>
+<body class="login-body">
+    <div class="login-card">
+        <img src="imagenes/profetamundial.png" alt="Profeta Mundial" class="logo">
+        <h2>Cuenta desactivada</h2>
+        <p style="color: #cbd5e1; text-align: center; margin-bottom: 1.5rem;">
+            Tu cuenta no está activa.<br>
+            Para acceder es necesario que la actives desde tu correo electrónico.<br>
+            <small>Si no encontrás el correo, revisá tu bandeja de correo no deseado.</small>
+        </p>
+        <a href="index.php" class="btn-home" style="display: inline-block;">Cerrar</a>
+    </div>
+</body>
+</html>
