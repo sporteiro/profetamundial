5e5aacdae64696ff9a59691c383276d7a660ef4f
sporteiro <seblash@gmail.com>
2026-05-10 09:42:23 +0100

    corrected empezar and no

--- no.php
+++ no.php
@@ -1,3 +1,84 @@
+<?php require_once('Connections/conexion.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+?>
+<?php
+// *** Validate request to login to this site.
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+$loginFormAction = $_SERVER['PHP_SELF'];
+if (isset($_GET['accesscheck'])) {
+  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+}
+
+if (isset($_POST['usuario'])) {
+  $loginUsername=$_POST['usuario'];
+  $password=sha1($_POST['contrasena']);
+  $MM_fldUserAuthorization = "";
+  $MM_redirectLoginSuccess = "empezar.php";
+  $MM_redirectLoginFailed = "no.php";
+  $MM_redirecttoReferrer = false;
+  mysql_select_db($database_conexion, $conexion);
+  
+  $LoginRS__query=sprintf("SELECT usuario, contrasena, activo FROM usuarios WHERE BINARY usuario=%s AND contrasena=%s",
+    GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
+   
+  $LoginRS = mysql_query($LoginRS__query, $conexion) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+  $filas=mysql_fetch_assoc($LoginRS);
+  $activo=$filas['activo'];
+  if ($activo=='no') {
+	include_once('desactivada.php');
+  }
+  else if ($loginFoundUser) {
+     $loginStrGroup = "";
+    
+    //declare two session variables and assign them
+    $_SESSION['MM_Username'] = $loginUsername;
+    $_SESSION['MM_UserGroup'] = $loginStrGroup;	      
+
+    if (isset($_SESSION['PrevUrl']) && false) {
+      $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
+    }
+     $consulta=mysql_query("UPDATE usuarios SET enlinea='si' WHERE usuario='".$loginUsername."'");
+    header("Location: " . $MM_redirectLoginSuccess );
+  }
+  else {
+    header("Location: ". $MM_redirectLoginFailed );
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
